a3974e807d18620c0d4ac1b2ad77764a5a8d9d83
Ulises Bojorquez Ortiz <ulibojorquez@gmail.com>
2024-10-17 15:57:48 -0600

    Responsive UI

---
+++ src/components/Announcement.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Announcement = () => {
+    return (
+        <div className="bg-white p-4 rounded-md">
+            <div className='flex items-center justify-between'>
+                <h1 className='text-xl font-semibold'>Announcements</h1>
+                <span className='text-xs text-gray-400'>View All</span>
+            </div>
+            <div className='flex flex-col gap-4 mt-4'>
+                <div className='bg-lamaSkyLight rounded-md p-4'>
+                    <div className='flex items-center justify-between'>
+                        <h2 className='font-medium'>sdloper impusem sadfasdf</h2>
+                        <span className='text-xs text-gray-400 bg-white rounded-md px-1 py-1'>2024-01-01</span>
+                    </div>
+                    <p className='text-sm text-gray-400 mt-1'>Loprem impusm ada da sasda sdas asd asd asdasdasdasdasd das ed</p>
+                </div>
+                <div className='bg-lamaPurpleLight rounded-md p-4'>
+                    <div className='flex items-center justify-between'>
+                        <h2 className='font-medium'>sdloper impusem sadfasdf</h2>
+                        <span className='text-xs text-gray-400 bg-white rounded-md px-1 py-1'>2024-01-01</span>
+                    </div>
+                    <p className='text-sm text-gray-400 mt-1'>Loprem impusm ada da sasda sdas asd asd asdasdasdasdasd das ed</p>
+                </div>
+                <div className='bg-lamaYellowLight rounded-md p-4'>
+                    <div className='flex items-center justify-between'>
+                        <h2 className='font-medium'>sdloper impusem sadfasdf</h2>
+                        <span className='text-xs text-gray-400 bg-white rounded-md px-1 py-1'>2024-01-01</span>
+                    </div>
+                    <p className='text-sm text-gray-400 mt-1'>Loprem impusm ada da sasda sdas asd asd asdasdasdasdasd das ed</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Announcement
---
+++ src/components/forms/ResultForm.tsx
@@ -0,0 +1,147 @@
+'use client'
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod"
+import InputField from "../InputField";
+import Image from "next/image";
+
+const schema = z.object({
+    username: z.string().min(3, { message: 'User name must be at least 3 characters long!' })
+    .max(20, { message: 'User name must be at monst 20 characters long!' }),
+    age: z.number().min(10),
+    email: z.string().email({message: 'Invalid email address!'}),
+    password: z.string().min(8, {message: 'Password must be at least 8 characters long!'}),
+    firstName: z.string().min(1, {message: 'First Name is required!'}),
+    lastName: z.string().min(1, {message: 'Last Name is required!'}),
+    phone: z.string().min(1, {message: 'Phone is required!'}),
+    address: z.string().min(1, {message: 'Address is required!'}),
+    bloodType: z.string().min(1, {message: 'Blood Type is required!'}),
+    birthday: z.date({message: 'Birthday is required!'}),
+    sex: z.enum(['male', 'female'], {message: 'Sex is required!'}),
+    img: z.instanceof(File, {message:'Image is required!'})
+})
+
+type Inputs = z.infer<typeof schema>
+
+const ResultForm = ({type, data}: {type: 'create' | 'update', data?: any}) => {
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm<Inputs>({
+        resolver: zodResolver(schema),
+    })
+
+    const onSubmit = handleSubmit(data => {
+        console.log(data)
+    })
+
+    return (
+        <form className="flex flex-col gap-8" onSubmit={onSubmit}>
+            <h1 className="text-xl font-semibold">Create new result</h1>
+            <span className="text-xs text-gray-400 font-medium">Authentication Information</span>
+            <div className="flex justify-between flex-wrap gap-4">
+                <InputField 
+                label="Username"
+                name="username"
+                register={register}
+                error={errors.username}
+                defaultValue={data?.username}
+                />
+                <InputField 
+                label="Email"
+                name="email"
+                type="email"
+                register={register}
+                error={errors.email}
+                defaultValue={data?.email}
+                />
+                <InputField 
+                label="Password"
+                name="password"
+                type="password"
+                register={register}
+                error={errors.password}
+                defaultValue={data?.password}
+                />
+            </div>
+            <span className="text-xs text-gray-400 font-medium">Personal Information</span>
+            <div className="flex justify-between flex-wrap gap-4">
+                <InputField 
+                label="First Name"
+                name="firstName"
+                register={register}
+                error={errors.firstName}
+                defaultValue={data?.firstName}
+                />
+                <InputField 
+                label="Last Name"
+                name="lastName"
+                register={register}
+                error={errors.lastName}
+                defaultValue={data?.lastName}
+                />
+                <InputField 
+                label="Phone"
+                name="phone"
+                register={register}
+                error={errors.phone}
+                defaultValue={data?.phone}
+                />
+                <InputField 
+                label="Address"
+                name="address"
+                register={register}
+                error={errors.address}
+                defaultValue={data?.address}
+                />
+                <InputField 
+                label="Blood Type"
+                name="bloodType"
+                register={register}
+                error={errors.bloodType}
+                defaultValue={data?.bloodType}
+                />
+                <InputField 
+                label="Birthday"
+                name="birthday"
+                register={register}
+                error={errors.birthday}
+                defaultValue={data?.birthday}
+                type="date"
+                />
+                <div className="flex flex-col gap-2 w-full md:w-1/4">
+                    <label className="text-xs text-gray-500">Sex</label>
+                    <select className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full" {...register('sex')} defaultValue={data?.sex}>
+                        <option value='male'>Male</option>
+                        <option value='female'>Female</option>
+                    </select>
+                    {errors.sex?.message && <p className="text-xs text-red-400">{errors.sex?.message.toString()}</p>}
+                </div>
+                <div className="flex flex-col gap-2 w-full md:w-1/4 justify-center">
+                    <label className="text-xs text-gray-500 flex items-center gap-2 cursor-pointer" htmlFor="img">
+                        <Image 
+                        src='/upload.png'
+                        alt=""
+                        width={28}
+                        height={28}
+                        />
+                        <span>Uload a photo</span>
+                    </label>
+                    <input 
+                    type="file"
+                    {...register('img')}
+                    className="hidden"
+                    id="img"
+                    />
+                    {errors.img?.message && <p className="text-xs text-red-400">{errors.img?.message.toString()}</p>}
+                </div>
+            </div>
+            <button type='submit' className="bg-blue-400 text-white p-2 rounded-md">{type === 'create' ? 'Create' : 'Update'}</button>
+        </form>
+    )
+}
+
+export default ResultForm
