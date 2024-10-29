be8fe3df323906b1c1ccbee719cbb4e791083c84
Ulises Bojorquez Ortiz <ulibojorquez@gmail.com>
2024-10-29 15:17:04 -0600

    Chart querys

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,15 +1,19 @@
 import Announcement from '@/components/Announcement'
-import BigCalendar from '@/components/BigCalendar'
+import BigCalendarContainer from '@/components/BigCalendarContainer'
+import { auth } from '@clerk/nextjs/server'
 import React from 'react'
 
 const TeacherPage = () => {
+
+  const {userId} = auth()
+
   return (
     <div className='p-4 flex gap-4 flex-col xl:flex-row flex-1'>
       {/* LEFT */}
       <div className='w-full xl:w-2/3'>
         <div className='h-full bg-white p-4 rounded-md'>
           <h1 className='text-xl font-semibold'>Schedule</h1>
-          <BigCalendar />
+          <BigCalendarContainer type='teacherId' id={userId!} />
         </div>
       </div>
       {/* RIGHT */}
